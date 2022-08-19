4cfefe74ab6d791f1f6c182ad48e5e84ac5d2949
buliangz <109450731+buliangz@users.noreply.github.com>
2022-08-19 17:22:14 +0800

    Add files via upload

---
+++ js/fonsszie.js
@@ -0,0 +1,15 @@
+function funs1(){
+    document.getElementById('jssize').style.fontSize='32px'; 
+    document.getElementById('bigsz').style.height='13000px'; 
+    document.getElementById('jssize').style.height='13000px'; 
+}
+function funs2(){
+    document.getElementById('jssize').style.fontSize='16px'; 
+    document.getElementById('bigsz').style.height='3700px'; 
+    document.getElementById('jssize').style.height='3700px';
+}
+function funs3(){
+    document.getElementById('jssize').style.fontSize='12px'; 
+    document.getElementById('bigsz').style.height='2300px'; 
+    document.getElementById('jssize').style.height='2300px'; 
+}
